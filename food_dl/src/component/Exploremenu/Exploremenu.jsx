fda253632640e34a13dff4a580b400f2065b15b2
Muniyasamy V <100552905+muniyasamy124@users.noreply.github.com>
2025-04-13 18:51:52 +0530

    Update Exploremenu.jsx

--- food_dl/src/component/Exploremenu/Exploremenu.jsx
+++ food_dl/src/component/Exploremenu/Exploremenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ExploreMenu.css";
+import "./Exploremenu.css";
 import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = ({ category, setCategory }) => {
